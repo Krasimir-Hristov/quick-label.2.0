e911dadf391633625e7d32d169a67971b6ddbeb9
krasi <krasimir.xristov@gmail.com>
2025-07-20 17:14:32 +0200

    feat: create Header component with Kölle Zoo branding and animal icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,41 @@
 import React from 'react';
+import { Bird, Bone, Cat, Dog, Fish, PawPrint, Rabbit, Snail, Turtle } from 'lucide-react';
 
 const Header = () => {
   return (
     <header className="bg-black py-8 shadow-lg border-b-5 border-[#a8c706]">
       <div className="container mx-auto px-4">
-        <div className="flex justify-center items-center">
+        <div className="flex justify-between items-center">
+          {/* Left paw prints */}
+          <div className="flex items-center space-x-4">
+
+          
+          <Dog size={100} className="text-[#a8c706]" />
+          <PawPrint size={30} className="text-[#a8c706] opacity-100" />
+          <PawPrint size={40} className="text-[#a8c706] opacity-100" />
+          <PawPrint size={50} className="text-[#a8c706] opacity-100" />
+          <PawPrint size={70} className="text-[#a8c706] opacity-100" />
+          <Bone size={110} className="text-[#a8c706]" />
+          </div>
+          
+          
+          {/* Logo */}
           <div className="text-center">
-            <h1 className="text-5xl font-bold flex flex-col tracking-wide">
-              <span style={{ color: '#a8c706' }}>KÖLLE</span>
-              <span style={{ color: '#a8c706' }} className='ml-3'>ZOO</span>
+            <h1 className="font-extrabold flex flex-col tracking-wide">
+              <span style={{ color: '#a8c706' }} className='text-7xl'>KÖLLE</span>
+              <span style={{ color: '#a8c706' }} className='text-7xl'>ZOO</span>
             </h1>
+          </div>
+          
+          {/* Right cat icon */}
+          <div className="flex items-center space-x-4">
+          <Fish size={110} className="text-[#a8c706] transform -scale-x-100" />
+
+            <PawPrint size={70} className="text-[#a8c706] opacity-100" />
+            <PawPrint size={50} className="text-[#a8c706] opacity-100" />
+            <PawPrint size={40} className="text-[#a8c706] opacity-100" />
+            <PawPrint size={30} className="text-[#a8c706] opacity-100" />
+            <Cat size={100} className="text-[#a8c706]" />
           </div>
         </div>
       </div>
