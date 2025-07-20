49192df4be399387c225bb3166d73a6dde9ed44d
krasi <krasimir.xristov@gmail.com>
2025-07-20 16:01:02 +0200

    feat: create header component with Kolle Zoo branding and styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-black py-8 shadow-lg">
+    <header className="bg-black py-8 shadow-lg border-b-5 border-[#a8c706]">
       <div className="container mx-auto px-4">
         <div className="flex justify-center items-center">
           <div className="text-center">
@@ -10,7 +10,6 @@ const Header = () => {
               <span style={{ color: '#a8c706' }}>KÖLLE</span>
               <span style={{ color: '#a8c706' }} className='ml-3'>ZOO</span>
             </h1>
-            <div className="mt-2 h-1 w-32 mx-auto" style={{ backgroundColor: '#a8c706' }}></div>
           </div>
         </div>
       </div>
